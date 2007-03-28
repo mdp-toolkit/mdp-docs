19b8ce906058f46becf53af991acd800cf1e5e05
Pietro Berkes <pietro.berkes@googlemail.com>
2007-03-28 16:43:04 +0000

    gen_logo update
    git-svn-id: https://mdp-toolkit.svn.sourceforge.net/svnroot/mdp-toolkit/mdp/trunk@171 c860887f-54fc-0310-8c92-9a9c51041e24

--- html_src/logo/gen_logo.py
+++ html_src/logo/gen_logo.py
@@ -7,7 +7,8 @@ fg_color = 'r'
 imgsize = (865, 400)
 imgorig = (50, 0)
 text = "MDP"
-N = 10000 # data points
+N = 5000 # data points per iteration
+NITER = 5 # number of iterations
 filling = 0.9 # probability that a data point belongs to the foreground
 max_nodes = 500
 new_image = False
@@ -40,24 +41,29 @@ x.shape = (imgsize[1], imgsize[0])
 
 # create 2-D data distribution correspondent to the image
 
-# shuffled lists of indices of the foreground and background pixels 
-idx_fg = x.nonzero()
-idx_bg = (1.-x).nonzero()
-idx_fg = zip(idx_fg[1], idx_fg[0])
-idx_bg = zip(idx_bg[1], idx_bg[0])
-numpy.random.shuffle(idx_fg)
-numpy.random.shuffle(idx_bg)
-N_fg = int(N*filling) 
+def data_generator(niter):
+    for i in range(niter):
+        print 'Iteration ', i
+        # shuffled lists of indices of the foreground and background pixels 
+        idx_fg = x.nonzero()
+        idx_bg = (1.-x).nonzero()
+        idx_fg = zip(idx_fg[1], idx_fg[0])
+        idx_bg = zip(idx_bg[1], idx_bg[0])
 
-# choose N_fg pixels from fg and N-N_fg pixels from bg
-idx = idx_fg[:N_fg] + idx_bg[:N-N_fg]
-numpy.random.shuffle(idx)
-data = numpy.array(idx).astype('d')
+        numpy.random.shuffle(idx_fg)
+        numpy.random.shuffle(idx_bg)
+        N_fg = int(N*filling) 
+
+        # choose N_fg pixels from fg and N-N_fg pixels from bg
+        idx = idx_fg[:N_fg] + idx_bg[:N-N_fg]
+        numpy.random.shuffle(idx)
+        yield numpy.array(idx).astype('d')
 
 print 'Learning neural gas.'
 # neural gas node
 gng = mdp.nodes.GrowingNeuralGasNode(max_nodes=max_nodes)
-gng.train(data)
+for data in data_generator(NITER):
+    gng.train(data)
 gng.stop_training()
 
 # plot resulting network
@@ -67,6 +73,7 @@ for e in gng.graph.edges:
     c0 = x[int(y0), int(x0)]<0.5 and bg_color or fg_color 
     x1, y1 = e.tail.data.pos
     c1 = x[int(y1), int(x1)]<0.5 and bg_color or fg_color
+    # determine edge color
     cline = c0==c1 and c0 or bg_color
     lines.extend(([x0,x1], [y0,y1], cline+'-',
                   [x0,x0], [y0,y0], c0+'.',
