e2c6bfb46d4427ace56399e93f6b5adab467765d
Niko Wilbert <mail@nikowilbert.de>
2010-03-15 18:04:09 +0100

    Changed the dimension of one node to make it more interesting.

--- backpropagation/demo_backprop.py
+++ backpropagation/demo_backprop.py
@@ -10,12 +10,12 @@ from perceptron import MPerceptronBiNode, BackpropBiNode
 # the switchboards are only dummys to show that it works
 switchboard1 = bimdp.hinet.BiSwitchboard(input_dim=18, connections=range(18),
                                          node_id="switchboard_1")
-layer1 = bimdp.hinet.CloneBiLayer(MPerceptronBiNode(input_dim=3),
+layer1 = bimdp.hinet.CloneBiLayer(MPerceptronBiNode(input_dim=3, output_dim=2),
                                   n_nodes=6, use_copies=True, node_id="layer_1")
 layer_flownode1 = bimdp.hinet.BiFlowNode(switchboard1 + layer1)
-switchboard2 = bimdp.hinet.BiSwitchboard(input_dim=6, connections=range(6),
+switchboard2 = bimdp.hinet.BiSwitchboard(input_dim=12, connections=range(12),
                                          node_id="switchboard_2")
-layer2 = bimdp.hinet.CloneBiLayer(MPerceptronBiNode(input_dim=3),
+layer2 = bimdp.hinet.CloneBiLayer(MPerceptronBiNode(input_dim=6),
                                   n_nodes=2, use_copies=True, node_id="layer_2")
 layer_flownode2 = bimdp.hinet.BiFlowNode(switchboard2 + layer2)
 # Note: bottom_node can't be set to container nodes, since the target value
