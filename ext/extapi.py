9d1ea208e6b7a90deedf69356a2d911d98b8eb45
Tiziano Zito <opossumnano@gmail.com>
2010-12-31 16:02:49 +0100

    the recent fix for extapi extension still failed to work.
    this fix is uglier than before, and probably it breaks on
    every machine which is not mine. but, on the other hand, no one can
    actually run epydoc on any other machine. so, all in all, this hack is
    not as bad.
    when do we leave epydoc and start using a new tool?

--- ext/extapi.py
+++ ext/extapi.py
@@ -73,7 +73,13 @@ def api_role(role, rawtext, text, lineno, inliner, options={}, content=[]):
         # ugly hack to add ../ when necessary
         current_source = inliner.document.current_source
         chunks = current_source.split('/')
-        nestedness = len(chunks) - chunks.index(source_base) - 2
+        print 'Hi'
+        print 'chunks: ', chunks
+        print 'source_base: ', source_base
+        try:
+            nestedness = len(chunks) - chunks.index(source_base) - 2
+        except ValueError:
+            nestedness = 1
         refuri = '../' * nestedness + uri
         node = nodes.reference(rawtext, name, refuri=refuri, **options)
     else:
